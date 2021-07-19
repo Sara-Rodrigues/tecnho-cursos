2f91bd5a20b9cea8a3b5bd379bba40aa2649dde3
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-07-19 19:40:29 -0300

    projeto header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-    </div>
+    <TheHeader />
+    <router-view></router-view>
   </div>
 </template>
+<script>
+import TheHeader from "@/components/TheHeader.vue";
 
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
 <style>
+body {
+  margin: 0px;
+  font-family: "Avenir", Arial, Helvetica, sans-serif;
+  color: #234;
+}
 
+ul {
+  list-style: none;
+  padding: 0px;
+  margin: 0px;
+}
+
+a {
+  font-weight: bold;
+  text-decoration: none;
+  color: #234;
+}
+
+a.router-link-active {
+  color: #4b8;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Contato from '../views/Contato.vue'
+import Cursos from '../views/Cursos.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
+  {
+    path: '/contato',
+    name: 'contato',
+    component: Contato
+  },
+  {
+    path: '/cursos',
+    name: 'cursos',
+    component: Cursos
+  }
 ]
 
 const router = new VueRouter({
